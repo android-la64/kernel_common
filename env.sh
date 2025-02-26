f5dd6253172c4140b80598e93741b9c340327153
Fuxin Zhang <fxzhang@ict.ac.cn>
2025-02-26 16:03:01 +0800

    config and some reference shell for cuttlefish kernel

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ARCH=loongarch
+export LLVM=~/aosp/prebuilts/clang/host/linux-x86/clang-r530567b/bin
+export PATH=~/aosp/prebuilts/clang/host/linux-x86/clang-r530567b/bin:$PATH
+export MOD_PATH="./dest"
---
+++ install.sh
@@ -0,0 +1,3 @@
+cp arch/loongarch/boot/vmlinuz.efi /home/zhangfuxin/aosp/device/google/cuttlefish_prebuilts/kernel/mainline-loongarch64/kernel-mainline 
+find ./ -name *.ko -exec cp {} /home/zhangfuxin/aosp/device/google/cuttlefish_prebuilts/kernel/mainline-loongarch64 \;
+
